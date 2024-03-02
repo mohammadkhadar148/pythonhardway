9aa4782072e3524331d8dfd08a26a058f1b3d0af
khadar <mohdkhadarmk999@gmail.com>
2024-03-02 16:30:27 +0530

    Exercise-11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+age = int(input())
+height = int(input())
+weight = int(input())
+print("How old are you?", age)
+print("How tall are you?", height)
+print("How much do you weigh?", weight)
+
+print("So, you are %r old, %r tall and %r heavy." % (age, height, weight))
