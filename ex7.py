b1103808965f179a2f6fc49a746f0fcc952c58fe
khadar <mohdkhadarmk999@gmail.com>
2024-03-02 16:28:54 +0530

    Exercise-7

---
+++ ex7.py
@@ -0,0 +1,13 @@
+print ("Mary had a little lamb.")
+print("It's fleece was white as %s." % 'snow')
+print("And everywhere that Mary went.")
+print("." * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+
+print(end1 + end2 + end3 + end4 + end5 + end6)
